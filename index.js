c4087473463d2e792949c6fd88f8a1e90555a523
irwansyah10010 <irwanbae33@gmail.com>
2023-02-16 23:06:49 +0700

    migration file redis

---
+++ index.js
@@ -0,0 +1,54 @@
+import  express  from 'express'
+import { createClient } from 'redis'
+
+const app = express()
+const port = 3001
+const client = createClient()
+
+// init redis
+client.connect().then(() => {
+  console.log('Redis client connected')
+}).catch((err)=>{
+  console.error('Error connecting to MongoDB', err)
+})
+
+// set redis manual
+client.set("name", "irwan")
+client.set("age", 26)
+client.set("hobies", JSON.stringify(["play games","sport"]))
+
+/* routing */
+
+// home
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// save data to redis
+app.get('/set/:key/:value', (req, res) => {
+    const key = req.params.key;
+    const value = req.params.value;
+  
+    client.set(key, value, (err, reply) => {
+      if (err) {
+        console.log(err);
+        res.send(err);
+      } else {
+        console.log(reply);
+        res.send(reply);
+      }
+    })
+})
+
+// get data from redist
+app.get('/get/:key', (req, res) => {
+  const key = req.params.key;
+
+  client.get(key).then((data) => res.send(data))
+})
+
+
+// run server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
