4a6df66492efe7772e170764af62d8c08ca332ee
irwansyah10010 <irwanbae33@gmail.com>
2023-02-18 17:45:13 +0700

    update manipulation redis

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 # redis-irwansyah-betest
-Project ini digunakan untuk mengambil data dari `cache redis` yang dibuat pada micro service
+Project ini digunakan untuk manipulasi data redis
 
 
-# berikut path url redis
+## berikut path url redis
+
+### get all key cache redis 
+`http://localhost:3000/keys` 
+method `GET`, mendapatkan semua kunci redis
 
 ### set cache redis 
-`http://localhost:3000/set/:key/:value` 
-method `GET`, mengatur data redis(:key -> diganti dengan kunci, :value -> diganti menjadi nilai)
+`http://localhost:3000/set/{key}/{value}` 
+method `GET`, mengatur data redis({key} -> diganti dengan kunci, {value} -> diganti dengan nilai)
 
 
 ### get cache redis 
-`http://localhost:3000/get/:key` 
-method `GET`, mencari data redis(:key -> diganti dengan kunci)
+`http://localhost:3000/get/{key}` 
+method `GET`, mencari data redis({key} -> diganti dengan kunci)
 
+
+### del cache redis
+`http://localhost:3000/del/{key}`
+method `GET`, menghapus data redis({key} -> diganti dengan kunci)
--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import  express  from 'express'
 import { createClient } from 'redis'
+import 'dotenv/config'
 
 const app = express()
 const port = 3001
-const client = createClient()
+const client = createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT
+})
 
 // init redis
 client.connect().then(() => {
   console.log('Redis client connected')
 }).catch((err)=>{
-  console.error('Error connecting to MongoDB', err)
+  console.error('Error connecting to redis', err)
 })
 
 // set redis manual
@@ -18,19 +22,20 @@ client.set("age", 26)
 client.set("hobies", JSON.stringify(["play games","sport"]))
 
 /* routing */
+// all key
+app.get('/keys',(req,res)=>{
+  client.keys('*').then((list)=>{
+    res.send(list)
+  })
+})
+
 // save data to redis
 app.get('/set/:key/:value', (req, res) => {
     const key = req.params.key;
     const value = req.params.value;
   
-    client.set(key, value, (err, reply) => {
-      if (err) {
-        console.log(err);
-        res.send(err);
-      } else {
-        console.log(reply);
-        res.send(reply);
-      }
+    client.set(key, value).then(()=>{
+      res.send(`${key} berhasil ditambahkan`)
     })
 })
 
@@ -41,6 +46,12 @@ app.get('/get/:key', (req, res) => {
   client.get(key).then((data) => res.send(data))
 })
 
+// delete data from redist
+app.get('/del/:key', (req, res) => {
+  const key = req.params.key;
+
+  client.del(key).then(() => res.send(`${key} berhasil dihapus`))
+})
 
 // run server
 app.listen(port, () => {
